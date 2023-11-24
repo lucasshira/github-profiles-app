96a6781fe9a221e2083123a768e5e6ed6bd32424
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2023-11-24 16:48:01 -0300

    comecando projeto

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const APIURL = 'https://api.github.com/users/';
+
+async function getUser(user) {
+    const resp = await fetch(APIURL + user);
+    const respData = await resp.json(user);
+
+
+
+    createUserCard(respData);
+}
+
+function createUserCard(user) {
+
+}
